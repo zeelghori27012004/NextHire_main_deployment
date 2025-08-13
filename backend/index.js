6e35dc6ba57d40b275b0e180193b78d9dae0e905
zeelghori27012004 <ghorizeeljkvrz@gmail.com>
2025-08-13 17:45:55 +0530

    added deployment

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: 'https://nexthire-main-deployment.onrender.com', 
     credentials: true
 };
 app.use(cors(corsOptions));
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,12 +1,12 @@
-const BASE_API_URL = "http://localhost:8000";
+// const BASE_API_URL = "http://localhost:8000";
 
-export const USER_API_END_POINT = `${BASE_API_URL}/api/user`;
-export const JOB_API_END_POINT = `${BASE_API_URL}/api/job`;
-export const APPLICATION_API_END_POINT = `${BASE_API_URL}/api/application`;
-export const COMPANY_API_END_POINT = `${BASE_API_URL}/api/company`;
+// export const USER_API_END_POINT = `${BASE_API_URL}/api/user`;
+// export const JOB_API_END_POINT = `${BASE_API_URL}/api/job`;
+// export const APPLICATION_API_END_POINT = `${BASE_API_URL}/api/application`;
+// export const COMPANY_API_END_POINT = `${BASE_API_URL}/api/company`;
 
 
-// export const USER_API_END_POINT="https://next-hire-backend-xi.vercel.app/api/user";
-// export const JOB_API_END_POINT="https://next-hire-backend-xi.vercel.app/api/job";
-// export const APPLICATION_API_END_POINT="https://next-hire-backend-xi.vercel.app/api/application";
-// export const COMPANY_API_END_POINT="https://next-hire-backend-xi.vercel.app/api/company";
+export const USER_API_END_POINT="https://nexthire-main-deployment.onrender.com/api/user";
+export const JOB_API_END_POINT="https://nexthire-main-deployment.onrender.com/api/job";
+export const APPLICATION_API_END_POINT="https://nexthire-main-deployment.onrender.com/api/application";
+export const COMPANY_API_END_POINT="https://nexthire-main-deployment.onrender.com/api/company";
